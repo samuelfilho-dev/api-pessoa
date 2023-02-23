56199a7884842b8874b256c97df01e7da7a9a31a
Samuel Marques <snamuelfilho2011@gmail.com>
2023-02-23 01:00:12 -0300

    Adição Da Exception Validação De Campos

--- People/src/main/java/com/attornatus/test/people/handler/RestExceptionHandler.java
+++ People/src/main/java/com/attornatus/test/people/handler/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import com.attornatus.test.people.exception.BadResquestExceptionDetails;
 import com.attornatus.test.people.exception.ExceptionDetalis;
 import com.attornatus.test.people.exception.ValidationExceptionDetalis;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -45,6 +46,21 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                         .mensagemDesenvolvedor(ex.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ValidationExceptionDetalis> handlerDataIntegrityViolationException(
+            DataIntegrityViolationException ex){
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetalis.builder()
+                        .horario(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .titulo("Bad Request Exception, Campo Invalido")
+                        .detalhes("Por Favor, Confira o(s) campo(s)")
+                        .mensagemDesenvolvedor(ex.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(
