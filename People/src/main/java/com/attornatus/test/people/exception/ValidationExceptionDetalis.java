82b6510d67749faedc2456fc956a3e5e0b60e16b
Samuel Marques <snamuelfilho2011@gmail.com>
2023-02-23 00:40:39 -0300

    Adição Da Exception Validação De Campos

--- People/src/main/java/com/attornatus/test/people/exception/ValidationExceptionDetalis.java
+++ People/src/main/java/com/attornatus/test/people/exception/ValidationExceptionDetalis.java
@@ -5,6 +5,6 @@ import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-public class BadResquestExceptionDetails extends ExceptionDetalis {
+public class ValidationExceptionDetalis extends ExceptionDetalis {
 
 }
--- People/src/main/java/com/attornatus/test/people/handler/RestExceptionHandler.java
+++ People/src/main/java/com/attornatus/test/people/handler/RestExceptionHandler.java
@@ -2,15 +2,18 @@ package com.attornatus.test.people.handler;
 
 import com.attornatus.test.people.exception.BadResquestException;
 import com.attornatus.test.people.exception.BadResquestExceptionDetails;
+import com.attornatus.test.people.exception.ValidationExceptionDetalis;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadResquestException.class)
     public ResponseEntity<BadResquestExceptionDetails> handlerBadResquestException(BadResquestException ex) {
@@ -23,5 +26,21 @@ public class RestExceptionHandler {
                         .mensagemDesenvolvedor(ex.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ValidationExceptionDetalis> handlerConstraintViolationException(
+            ConstraintViolationException ex) {
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetalis.builder()
+                        .horario(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .titulo("Bad Request Exception, Campo Invalido")
+                        .detalhes("Por Favor, Confira o(s) campo(s)")
+                        .mensagemDesenvolvedor(ex.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+
 
 }
