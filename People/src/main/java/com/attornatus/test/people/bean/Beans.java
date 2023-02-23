b4c6457e301260ffc1287759494336a2cdb30a99
Samuel Marques <snamuelfilho2011@gmail.com>
2023-02-22 23:30:43 -0300

    Adição do Mapeamento Automatico

--- People/src/main/java/com/attornatus/test/people/bean/Beans.java
+++ People/src/main/java/com/attornatus/test/people/bean/Beans.java
@@ -2,6 +2,7 @@ package com.attornatus.test.people.bean;
 
 import com.attornatus.test.people.model.Endereco;
 import com.attornatus.test.people.model.Pessoa;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,5 +18,10 @@ public class Beans {
     @Bean
     public Endereco endereco(){
         return new Endereco();
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
